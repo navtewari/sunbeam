888a7e6d1ae210ae7e32132f39bde828f9eeeb20
Dr. Nitin Deepak <nitin.d12@gmail.com>
2018-02-06 23:58:51 +0530

    updates
    updates

--- dpanel/_assets_/newsletters/fronts/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-	session_start();
-	session_destroy();
-	header('http://www.amrapali.ac.in');
-?>
--- dpanel/application/views/templates/navigation.php
+++
@@ -1,5 +0,0 @@
-<nav class="navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-            <?php $this->load->view('templates/header_navigate') ;?>
-            <!-- /.navbar-top-links -->
-            <?php $this->load->view('templates/side_navigation'); ?>
-        </nav>
