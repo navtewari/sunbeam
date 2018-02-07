06374afc423d3e435abfe40e3060ee475d5e5e72
Dr. Naveen Tewari <navtewari@gmail.com>
2018-02-07 20:45:36 +0530

    up
    up

--- application/views/contact.php
+++ application/views/contact.php
@@ -7,13 +7,12 @@
         <div class="main-head-section">
             <h2>Contact</h2>
             <div class="contact-map">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d424396.3176723366!2d150.92243255000002!3d-33.7969235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b129838f39a743f%3A0x3017d681632a850!2sSydney+NSW%2C+Australia!5e0!3m2!1sen!2sin!4v1431587453420" width="100%" height="151px" frameborder="0" style="border:0"></iframe>
+                <iframe width="600" height="450" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=The%20Sunbeam%20Senior%20Secondary%20School%2C%20haldwani&key=AIzaSyCLhJL8fD5gTA-ZZthonhF73Yhhg67QaSs" allowfullscreen></iframe>
             </div>
         </div>
         <div class="contact_top">			 		
             <div class="col-md-8 contact_left">
-                <h4>Below Type Here</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tincidunt dolor et tristique bibendum. Aenean sollicitudin vitae dolor ut posuere.</p>
+                <h4>Quick Query</h4>                
                 <form>
                     <div class="form_details">
                         <input type="text" class="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {
@@ -38,12 +37,15 @@
             <div class="col-md-4 company-right">
                 <div class="company_ad">
                     <h3>Address</h3>
-                    <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit velit justo.</span>
-                    <address>
-                        <p>email : <a href="mailto:example@mail.com">info@display.com</a></p>
-                        <p>phone : 1.306.222.4545</p>
-                        <p>222 2nd Ave South</p>
-                        <p>Saskabush, SK   S7M 1T6</p>									 	 	
+                    <span>The Sunbeam Senior Secondary School</span>
+                    <address>                        
+                        <p>Himmatpur Talla, </p>
+                        <p>Haldwani- 263139, District- Nainital</p>
+                        <p>Uttarakhand, India</p>
+                        <p>&nbsp;</p>
+                        <p><b>email :</b> <a href="#">thesunbeamschool2006@gmail.com<br>
+                            jcpant.20002002@gmail.com </a></p>
+                        <p><b>phone :</b> 9410112596, 9897910445</p>
                     </address>
                 </div>
             </div>	
