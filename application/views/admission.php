16d8d553837ef8350173fad02330ccc8fe5f6bdd
Dr. Nitin Deepak <nitin.d12@gmail.com>
2018-02-16 23:10:55 +0530

    updates
    updates

--- application/views/admission.php
+++ application/views/admission.php
@@ -4,10 +4,12 @@
 <div class="about-sec">
     <div class="container">
         <div class="about-head">
-            <h2>Admission Information</h2>                    
+            <h2>Admission Information</h2>       
+            <h6 style="background: #FE795D; padding: 5px; border-radius: 5px; float: left"><a href="<?php echo base_url('/assets/dwnlds/admform.pdf');?>" target="_blank" style="color: #ffffff">Download Admission Form</a></h6>     
+            <br>        
         </div>
         <div class="video-grid">            
-            <div class="col-md-7 video-info">                
+            <div class="col-md-7 video-info">
                 <h4>Age Criteria for Admission</h4>
                 <ul class="ftr-grid2" style="text-align: justify; line-height:1.8em;">
                     <li><b>Nursery:</b> The child should be 2+ on 30th July of the corresponding year.</li>
@@ -18,12 +20,34 @@
                 </ul>
                 <p>&nbsp;</p>
                 <h4>Admission Procedure</h4>
-                <p>Little Spark follows CBSE (Centeral Board of Secondary Education, New Delhi) Procedure. </p>
+                <p>The Sunbeam follows CBSE (Centeral Board of Secondary Education, New Delhi) Procedure. </p>
             </div>
             <div class="col-md-5 video">
                 <img src="<?php echo base_url('assets/images/11.jpg');?>" class="img-responsive">
             </div>
             <div class="clearfix"></div>
+            <div class="col-md-12 video-info">
+                <style>
+                    li{
+                        padding: 2px;
+                    }
+                </style>
+                <h4>Our rules expected to be followed by our students as follows: </h4>
+                <p>
+                    <ul class="ftr-grid2" style="text-align: justify">
+                        <li>All the students should come clean and tidy in their school uniform. All are obliged to wear the uniform under the penalty of execution from the class.</li>
+                        <li>Punctual and regular attendance is insisted upon, if a student is absent due to illness or any other valid reasons, the Principal should be informed about it immediately by a note from the guardian.</li>
+                        <li>Absence from school prior to a test on Saturdays that are marked as class days will adversely affect the student’s grade in the test.</li>
+                        <li>If a student continues to be absent without a leave note for more than 10 working days in succession, his/her name will be struck off the register. Re-admission, if granted, will be done on payment of re-admission fee.</li>
+                        <li>All are expected to attend class on the opening day after each of the vacations. Those absent without prior leave will be fined.</li>
+                        <li>A student returning to school after suffering from an infection or contagious disease should produce a doctor’s certificate permitting him /her to rejoin class.</li>
+                        <li>Student cannot be admitted to the school without transfer certificate of the school last attended.</li>
+                        <li>Admissions are granted from Nursery to XII. Admission is given on the basis of a written test followed by an interview, conducted by the school authorities. However for Nursery and LKG classes mental growth is judged through certain puzzles &amp; equipments.</li>
+                        <li>A child seeking admission must produce TC or a date of birth certificate issued by Municipal Corporation or Gram Panchayat. The school reserves the right of refusing admission without assigning any reason for doing so.</li>
+                    </ul>
+                </p>
+                <h6 style="background: #FE795D; padding: 5px; border-radius: 5px; float: right"><a href="<?php echo base_url('/assets/dwnlds/admform.pdf');?>" target="_blank" style="color: #ffffff">Download Admission Form</a></h6>  
+            </div>
         </div>        
     </div>
 </div>
--- application/views/features.php
+++ application/views/features.php
@@ -8,26 +8,32 @@
         </div>
         <div class="video-grid">            
             <div class="col-md-7 video-info">                
+                <p>
+                    The Sunbeam Sr. Sec. School is an English Medium School for boys and girls from Nursery to Class XII. The school is affiliated to the Central Board of Secondary Education (CBSE), Delhi.<br>
+We believe that the early education which lays the foundation stone in a child education, is very significant for his/her success in future. So, it must be solid and sound.
+
+                </p>
                 <h4>The salient features of our School include the following –</h4>
-                <ul class="ftr-grid2">
-                    <li>The Sunbeam School is reckoned for trust and quality </li>
-                    <li>Lush <span style="color: #009000">Green</span> Campus </li>
-                    <li>State of the art infrastructure </li>
-                    <ul style='font-weight: bold; color: #008080'>
-                        <li>Dedicated transport </li>
-                        <li>Basketball, tennis and badminton courts </li>
-                        <li>Premium furniture </li>                        
-                    </ul>                                        
-                    <li>Music ,dance classes </li>
-                    <li>Separate transport for kindergarten children </li>
-                    <li>Highly qualified and <b>experienced faculty </b></li>
-                    <li>Regular parent teacher interaction to keep parents updated about their child’s performance </li>
-                    <li>Students take part in social service </li>
+                <ul class="ftr-grid2" style="text-align: justify">
+                    <li><b>Teacher and Classes - </b>For giving individual attention we intend to limit the number of students in each class. We have qualified teachers specialized in particular subject. Children are encouraged for good hand writing. Teaching aids are ready accordingly to the need of subjects.</li>
+                    <li><b>Parents-Teacher Meeting - </b> There is the provision of parents- Teacher meeting every month so that the parents may discuss their problems freely.</li>
+                    <li><b>Library - </b> The School has the provision of well equipped library. Books accordingly to the standard and other reading material are available for the children.</li>
                 </ul>
-              
             </div>
             <div class="col-md-5 video">
                 <img src="<?php echo base_url('assets/images/3.jpg');?>" class="img-responsive">
+            </div>
+            <div class="col-md-12 video-info">
+                <ul class="ftr-grid2" style="text-align: justify">
+                    <li><b>Computer - </b> From Class I onwards Computer education is prescribed in the curriculum. We have the well equipped computer room for the purpose.</li>
+                    <li><b>Fee Schedule - </b> School fee has to be paid by the 15th of each month, failing to do so the parents will be charged RS. 10/- late payment fee, but the fee must be paid by the end of the month.</li>
+                    <li><b>Emphasis on Play Way - </b> Our School puts more stress on play &amp; play activities. All lesions are accomplished by songs, gestures, and movements.</li>
+                    <li><b>House System - </b> The House System fosters a sprit of co-operation and competition among students. Each house will be put under the charge of a house master and a teacher who guide the students.</li>
+                    <li><b>Tests and Examinations - </b> During Academic session the school will hold regular tests besides other examinations. Promotion is granted on the combined result of all the test and examination, which also includes internal assessment of the child.</li>
+                    <li><b>Assistance for home works or weak subjects - </b> Private tuition are strongly discouraged. Our school will however, provide extra time, apart from school hours, to complete their homework etc. for interested parents.</li>
+                    <li><b>Excursion - </b> The students are taken on excursions a least once during the academic year, followed by the picnic, movies, mountain tracking, etc.</li>
+                    <li><b>Transport - </b> Transport facility is available on request to transport children to school on a specified route.</li>
+                </ul>
             </div>
             <div class="clearfix"></div>
         </div>        
