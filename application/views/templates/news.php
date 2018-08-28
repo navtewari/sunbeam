aca8955146fb1928c2d0a5dd65626adc3582c1ce
Dr. Nitin Deepak <nitind12@users.noreply.github.com>
2018-08-28 16:08:12 +0530

    updates
    updates

--- application/views/templates/news.php
+++ application/views/templates/news.php
@@ -7,7 +7,7 @@
                     <p style="margin-bottom: 5px;"><?php echo $item->NEWS; ?></p>
                     <div title="Source Title" style="color: #ffffff; float: left">
                         <?php if ($item->PATH_ATTACH != 'x') { ?>
-                            <a href="<?php echo ADMIN___ . '/assets/newsdetail/' . $item->PATH_ATTACH; ?>" target="_blank" style="text-decoration: none; color:#fff; background: #9090DD; padding: 2px; border-radius: 3px; font-size: 10px">click for detail</a>
+                            <a href="<?php echo ADMIN___ . '/_assets_/newsdetail/' . $item->PATH_ATTACH; ?>" target="_blank" style="text-decoration: none; color:#fff; background: #9090DD; padding: 2px; border-radius: 3px; font-size: 10px">click for detail</a>
                         <?php } ?>
                     </div>
                     <div style="color:#0086B3; font-size: 10px; float: right; margin-top: 10px"><?php echo $item->DATE_ . " | " . $item->TIME_; ?></div>
